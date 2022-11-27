ad04e889494486610f598e245f4c39921eeb161b
Matyas Novak <logik@centrum.cz>
2022-11-27 21:52:50 +0100

    Compatibility of the python build process

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-from distutils.extension import Extension
 import numpy as np
 import os.path
+
+try:
+  import numpy.distutils.system_info as np_config
+except ImportError:
+  """ In my system, this gives wrong blas linking info """
+  import numpy.__config__ as np_config
+
+get_config = np_config.get_info
+config = get_config('lapack_opt') or get_config('lapack_ilp64_opt')
+
+if not config:
+  raise Exception('Cannot determine LAPACK libraries')
 
 curdir = os.path.dirname(os.path.realpath(__file__))
 ext_modules = [ Extension(
     name="adaptive_anderson_solver",
     sources=["adaptive_anderson_solver.pyx"],
-    libraries=["adaptive_anderson_solver"] + np.__config__.blas_opt_info['libraries'],
+    libraries=["adaptive_anderson_solver"] + config['libraries'],
     include_dirs=[np.get_include(), os.path.dirname(__file__)],
-    library_dirs=["./lib/"] + np.__config__.blas_opt_info['library_dirs'],
+    library_dirs=["./lib/"] + config['library_dirs'],
     extra_compile_args=["-O3"],
     extra_link_args=["-Wl,-rpath,./lib:"+curdir]
 )]
