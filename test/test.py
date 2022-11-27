419f9a69da08bdde0912f3a369971070d16dcc3a
Matyas Novak <novakmat@fzu.cz>
2022-11-27 17:25:53 +0100

    pytest compatibility

--- test/test.py
+++ test/test.py
@@ -1,27 +1,34 @@
 import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-import adaptive_anderson_solver
 import numpy as np
+import os
 
-def fce(x):
-    fce = lambda x,y: [x**2-10+y, y**2-5]
-    return np.array(fce(*x))
+def test():
+    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+    import adaptive_anderson_solver
 
-out=adaptive_anderson_solver.solve(fce, np.array([4.,4]), tolerance=1e-5)
-assert np.linalg.norm(fce(out))< 1e-5
-out=adaptive_anderson_solver.solve(fce, out, weights=np.array([1.,2]))
 
-try:
-    out=adaptive_anderson_solver.solve(fce, out, weights=np.array([1.,-2.]), tolerance=1e-5)
-    ok=True
-except ValueError:
-    ok=False
 
-assert ok==False
+    def fce(x):
+        fce = lambda x,y: [x**2-10+y, y**2-5]
+        return np.array(fce(*x))
 
-x=np.array([4.,4.])
-aa=adaptive_anderson_solver.AdaptiveAndersonSolver(x, tolerance=1e-5)
-while not aa.step(fce(x),x):
-    pass
-assert np.linalg.norm(fce(x))< 1e-5
+    out=adaptive_anderson_solver.solve(fce, np.array([4.,4]), tolerance=1e-5)
+    assert np.linalg.norm(fce(out))< 1e-5
+    out=adaptive_anderson_solver.solve(fce, out, weights=np.array([1.,2]))
+
+    try:
+        out=adaptive_anderson_solver.solve(fce, out, weights=np.array([1.,-2.]), tolerance=1e-5)
+        ok=True
+    except ValueError:
+        ok=False
+
+    assert ok==False
+
+    x=np.array([4.,4.])
+    aa=adaptive_anderson_solver.AdaptiveAndersonSolver(x, tolerance=1e-5)
+    while not aa.step(fce(x),x):
+        pass
+    assert np.linalg.norm(fce(x))< 1e-5
+
+if __name__ == "__main__":
+    test()
